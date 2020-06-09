2a4499df5c78d832c23e0832467805983182a241
Egor Shitikov <egorshitikov@gmail.com>
2020-06-09 15:28:29 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,10 +14,7 @@ cd cloud9-magent
 
 sudo cp php_fpm_magento.conf /etc/nginx/conf.d/,
 sudo cp default_magento.conf /etc/nginx/default.d/,
-
-
-# fix root" directive is duplicate in /var/www/html/magento/nginx.conf.sample:29
-sudo sed -e '/root/ s/^#*/#/' -i /etc/nginx/nginx.conf
+sudo cp nginx.conf /etc/nginx/,
 
 ## put sorce code ther - /var/www/html/magento/
 ## sudo git clone https://github.com/User/repo /var/www/html/magento
