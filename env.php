ef7456158523fc3e1acb5a69c384760cdb2d33b4
Egor Shitikov <egorshitikov@gmail.com>
2020-06-09 18:11:18 -0700

    Create env.php

---
+++ env.php
@@ -0,0 +1,117 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'fdfcddb19a3f5ec12c5f06bb872b3e90920d43310f1d0411df337182980bfb8697d54a9f4286ab0ddcdce152f9bafa51'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'magento2',
+                'username' => 'root',
+                'password' => 'magento2',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'production',
+    'session' => [
+        'save' => 'redis',
+        'redis' => [
+            'host' => 'localhost',
+            'port' => '6379',
+            'password' => '',
+            'timeout' => '2.5',
+            'persistent_identifier' => '',
+            'database' => '2',
+            'compression_threshold' => '2048',
+            'compression_library' => 'gzip',
+            'log_level' => '3',
+            'max_concurrency' => '6',
+            'break_after_frontend' => '5',
+            'break_after_adminhtml' => '30',
+            'first_lifetime' => '600',
+            'bot_first_lifetime' => '60',
+            'bot_lifetime' => '7200',
+            'disable_locking' => '1',
+            'min_lifetime' => '60',
+            'max_lifetime' => '2592000'
+        ]
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '5e9_',
+                'backend' => 'Cm_Cache_Backend_Redis',
+                'backend_options' => [
+                    'server' => 'localhost',
+                    'database' => '0',
+                    'port' => '6379',
+                    'password' => '',
+                    'compress_data' => '1',
+                    'compression_lib' => '',
+                    'preload_keys' => [
+                        '061_EAV_ENTITY_TYPES',
+                        '061_GLOBAL_PLUGIN_LIST',
+                        '061_DB_IS_UP_TO_DATE',
+                        '061_SYSTEM_DEFAULT'
+                    ]
+                ]
+            ],
+            'page_cache' => [
+                'id_prefix' => '5e9_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1,
+        'fishpig_wordpress' => 1
+    ],
+    'install' => [
+        'date' => 'Mon, 05 Aug 2019 06:13:43 +0000'
+    ],
+    'dev' => [
+        'debug' => [
+            'debug_logging' => 0
+        ]
+    ],
+    'db_logger' => [
+        'output' => 'disabled',
+        'log_everything' => 1,
+        'query_time_threshold' => '0.001',
+        'include_stacktrace' => 1
+    ]
+];
