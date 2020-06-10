d55754ed5b2b56f7fbcbde78f79e7799140338b4
Egor Shitikov <egorshitikov@gmail.com>
2020-06-09 21:03:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 yum remove -y mysql55 mysql55-common mysql55-libs mysql55-server php56* 
 sudo yum install -y nginx mysql57-server php72-mysqlnd
 sudo yum install -y php72 php72-pear php72-common php72-devel php72-mysqlnd php72-opcache php72-xml php72-gd php72-soap php72-redis php72-bcmath php72-intl php72-mbstring php72-json php72-iconv php72-fpm php72-zip
+sudo yum install -y redis
 
+sudo service redis start
 sudo service mysqld start
 sudo service nginx start
 sudo service php-fpm start
@@ -34,3 +36,9 @@ ln -s /var/www/html/magento/ ~/environment/magento
 
 ## put sorce code ther - /var/www/html/magento/
 ## sudo git clone https://github.com/User/repo /var/www/html/magento
+##
+## composer install
+## UPDATE core_config_data set value = "0" where path like "%web/secure/use_in%";
+## UPDATE core_config_data set value = "https://domain/" where path like "%base_url%";
+## sudo bin/magento setup:static-content:deploy
+## sudo bin/magento setup:di:compile
